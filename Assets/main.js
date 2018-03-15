8e0ad4fc7e844d4bfa25486cb84a9b7855279cb2
Olubukola Y. Ogundipe <buki@Olubukolas-MacBook-Pro.local>
2018-03-14 19:51:55 -0500

    crystal crazy

---
+++ Assets/main.js
@@ -0,0 +1,36 @@
+//document loads
+
+
+// variables
+var totalScore = null;
+var randomNumber = '';
+var wins = '';
+var losses = '';
+// random value 1 - 12
+var red = '';
+var purple = '';
+var yellow = '';
+var blue = '';
+
+
+
+
+
+
+// functions
+    // calculating wins
+
+    // calculating losses
+
+    // calculating total score
+
+    // giving a random number
+
+
+
+
+
+
+
+// events
+
